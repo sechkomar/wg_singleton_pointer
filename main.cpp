a376f0f4430d717e63e2dc683da4e2bccd4ab96b
sechko_git <sechko.mar@yandex.ru>
2017-11-10 02:01:42 +0300

    delete comments

--- main.cpp
+++ main.cpp
@@ -9,24 +9,6 @@ void fun(smart_pointer<Prob> p) {
 
 
 int main() {
-//    Singleton &singleton = Singleton::instance();
-//    smart_pointer<Prob> smart_prob(new Prob(1, "name1"));
-
-//    auto *prob = new Prob(5, "fg");
-//    smart_pointer<Prob> ptr(prob);
-
-
-//    ptr.get_object()->get_description();
-
-//    smart_pointer<Prob> ptr1(prob);
-//    ptr1.get_object()->get_description();
-
-
-//    std::shared_ptr<Prob> shared_ptr(prob);
-//    std::shared_ptr<Prob> ptr = shared_ptr;
-
-//    shared_ptr.operator->()->get_description();
-
 
     auto *prob = new Prob(567, "ddddd");
 
